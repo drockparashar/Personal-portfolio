b7e1d563feadd45977e3631a6c74c5a3e8a08118
drockparashar <pranshu007parashar@gmail.com>
2023-12-31 02:39:56 +0530

    Added herobox

---
+++ README.md
@@ -0,0 +1,3 @@
+background:Midnight Blue (#2c3e50)
+buttons:Turquoise (#1abc9c)
+text:#CCCCCC
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Hero from './Hero'
+import Projects from './projects'
+
+
+function App() {
+
+  return (
+    <>
+      <Hero/>
+      <Projects></Projects>
+    </>
+  )
+}
+
+export default App
